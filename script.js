b09c35e9cab2e96ebe24315e72d51e0f068543e5
Gabriel R Barbosa <110126012+gabrielrbarbosa23@users.noreply.github.com>
2024-10-11 15:39:13 -0300

    improving the css and scripts

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+/*--------------------------------------------- img ------------------------------------------------*/
+function openImg() {
+    Swal.fire({
+        imageUrl: "assets/img/me_full.webp",
+        showCloseButton: true,
+        showConfirmButton: false,
+        imageWidth: 300,
+        imageHeight: 400,
+        imageAlt: "Custom image",
+        showClass: {
+            popup: 'swal2-no-padding'
+        },
+        customClass: {
+            popup: 'image-only-modal'
+        }
+    });
+}
+
 /*--------------------------------------------- navbar ------------------------------------------------*/
 function home() {
     $('#home').show();
@@ -37,4 +55,31 @@ function contact() {
     $('#about').hide();
     $('#projects').hide();
     $('#resume').hide();
+}
+
+/*--------------------------------------------- projects ------------------------------------------------*/
+function fullstack() {
+    $('#frontend').show();
+    $('#backend').show();
+    $('#nav-fullstack').addClass('active');
+    $('#nav-frontend').removeClass('active');
+    $('#nav-backend').removeClass('active');
+}
+
+function frontend() {
+    $('#frontend').show();
+    $('#fullstack').hide();
+    $('#backend').hide();
+    $('#nav-fullstack').removeClass('active');
+    $('#nav-backend').removeClass('active');
+    $('#nav-frontend').addClass('active');
+}
+
+function backend() {
+    $('#backend').show();
+    $('#fullstack').hide();
+    $('#frontend').hide();
+    $('#nav-fullstack').removeClass('active');
+    $('#nav-frontend').removeClass('active');
+    $('#nav-backend').addClass('active');
 }
